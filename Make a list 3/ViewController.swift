6e35fcf34b43ccaf38d156282b68c2f013b7553e
Rob_inHood <pokhrelrob@gmail.com>
2017-01-09 13:32:47 -0600

    just table here

---
+++ Make a list 3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Make a list 3
+//
+//  Created by Mac User on 1/9/17.
+//  Copyright © 2017 UglyFish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
